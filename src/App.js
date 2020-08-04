714aa477f6cbc47ead0ae3016e6e1a4d861948a7
Pramuka Jayathilaka <opasan4all@gmail.com>
2020-08-04 13:07:54 +0530

    fifth commit
    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Todos from './components/Todos.js';
 
+
 function App() {
+  let [todos, setTodos] = useState(
+    [
+      { id:1, title: 'Take out the trash' },
+      { id:2, title: 'Dinner with wife' },
+      { id:3, title: 'Meeting with boss' }
+    ]);
+
+    console.log("this is a console log comment");
+    //console.log(this.use
+    
   return (
     <div className="App">
       <Todos />
